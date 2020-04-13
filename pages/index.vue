c545f11c9f5a7b02af30fd645e3909713ea8832a
Marcus Camargo <mvocamargo@gmail.com>
2020-04-13 19:01:41 -0300

    Adding async/await to the list of events

--- pages/index.vue
+++ pages/index.vue
@@ -15,20 +15,18 @@ export default {
   components: {
     EventCard
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:8080/events')
-      .then((response) => {
-        return {
-          events: response.data
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:8080/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again later'
       })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. Please try again later'
-        })
-      })
+    }
   },
   head() {
     return {
